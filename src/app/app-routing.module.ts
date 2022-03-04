465536a6a713f6483d1f67c29213f3d43a771aba
Annween <49524082+Annween@users.noreply.github.com>
2022-03-04 17:02:10 +0100

    add html to tab1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'posts',
+    loadChildren: () => import('./posts/posts.module').then( m => m.PostsPageModule)
+  },
+  {
+    path: 'post-detail/:id',
+    loadChildren: () => import('./post-detail/post-detail.module').then( m => m.PostDetailPageModule)
   }
+
 ];
 @NgModule({
   imports: [
